544fea589d183d5ebe78fedc39648e8ea374f48f
Starrie Williamson <ssw3r@virginia.edu>
2019-07-20 19:20:45 -0400

    upload songs

--- music-player-1.0.1/demo/js/myplaylist.js
+++ music-player-1.0.1/demo/js/myplaylist.js
@@ -9,14 +9,14 @@
 var myPlaylist = [
 
     {
-        mp3:'mix/1.mp3',
-        oga:'mix/1.ogg',
+        mp3:'../mix/1.mp3',
+        oga:'../mix/1.ogg',
         title:'Sample',
         artist:'Sample',
         rating:4,
         buy:'#',
         price:'0.99',
         duration:'0:30',
-        cover:'mix/1.png'
+        cover:'../mix/1.png'
     }
 ];
